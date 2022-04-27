373ca94724fda99b1feb1a04cc660c9dd72d12fe
Haneet Singh Chhabra <haneet555@gmail.com>
2022-04-27 18:50:14 +0530

    login otp

--- src/App.js
+++ src/App.js
@@ -1,125 +1,67 @@
+import React, { Component } from 'react';
+import firebase from './config/firebase'
+import './App.css';
+import { isElementOfType } from 'react-dom/test-utils';
 
-import React, { Component } from "react";
-import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-//import PrimaryButton from "./shared/button";
-import firebase from "firebase/app";
-import "firebase/auth";
-{/*import GoogleLogin from "./google-login";*/}
-
-export default class PhoneLogin extends Component {
-  constructor() {
+export default class App extends Component{
+  constructor(){
     super();
-    this.state = {
-      form: true,
-      alert: false,
-    };
+    this.state={
+      phonenumber:""
+    }
   }
-
-  onChangeHandler = (event) => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  setUpRecaptcha = () => {
-    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
-      "recaptcha-container",
-      {
-        size: "invisible",
-        callback: function (response) {
-          console.log("Captcha Resolved");
-          this.onSignInSubmit();
-        },
-        defaultCountry: "IN",
-      }
-    );
-  };
-
-  onSignInSubmit = (e) => {
-    e.preventDefault();
-    this.setUpRecaptcha();
-    let phoneNumber = "+91" + this.state.mobile;
-    console.log(phoneNumber);
-    let appVerifier = window.recaptchaVerifier;
-    firebase
-      .auth()
-      .signInWithPhoneNumber(phoneNumber, appVerifier)
-      .then(function (confirmationResult) {
+  setUpReCaptcha=()=>{
+  window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
+    'size': 'invisible',
+    'callback': function(response) {
+      // reCAPTCHA solved, allow signInWithPhoneNumber.
+      this.onSignInSubmit();
+    }
+  });
+}
+onSignInSubmit=()=>{
+  
+  this.setUpReCaptcha();
+  var num = this.state.phonenumber;
+  var n = num.toString();
+  console.log(typeof n)
+  var phoneNumber = "+91"+n;
+  var appVerifier = window.recaptchaVerifier;
+  firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+  .then(function (confirmationResult) {
         // SMS sent. Prompt user to type the code from the message, then sign the
         // user in with confirmationResult.confirm(code).
         window.confirmationResult = confirmationResult;
-        // console.log(confirmationResult);
-        console.log("OTP is sent");
-      })
-      .catch(function (error) {
-        console.log(error);
+        var code = window.prompt("Enter OTP");
+        confirmationResult.confirm(code).then(function (result) {
+  // User signed in successfully.
+  var user = result.user;
+  // ...
+  console.log("user is signed in")
+}).catch(function (error) {
+  // User couldn't sign in (bad verification code?)
+  // ...
+});
+      }).catch(function (error) {
+        // Error; SMS not sent
+        // ...
       });
-  };
-
-  onSubmitOtp = (e) => {
-    e.preventDefault();
-    let otpInput = this.state.otp;
-    let optConfirm = window.confirmationResult;
-    // console.log(codee);
-    optConfirm
-      .confirm(otpInput)
-      .then(function (result) {
-        // User signed in successfully.
-        // console.log("Result" + result.verificationID);
-        let user = result.user;
-      })
-      .catch(function (error) {
-        console.log(error);
-        alert("Incorrect OTP");
-      });
-  };
-
-  render() {
-    return (
+    }
+    render(){
+      console.log(this.state.phonenumber)
+      return (
+    <div className="App">
+      {/* <h1>Phone Auth</h1> */}
+      <div id="recaptcha-container"></div>
       <div>
-        <Container fluid="sm" className="mt-3">
-          <Row className="justify-content-center">
-            <Col xs={12} md={6} lg={5}>
-              <h2 className="mb-3">Login</h2>
-              <Form className="form" onSubmit={this.onSignInSubmit}>
-                <div id="recaptcha-container"></div>
-                <Form.Group>
-                  <Form.Control
-                    type="number"
-                    name="mobile"
-                    placeholder="Mobile Number"
-                    onChange={this.onChangeHandler}
-                    required
-                  />
-                </Form.Group>
-                <button  type="submit" >submit </button>
-              </Form>
-            </Col>
-          </Row>
-          <Row className="justify-content-center">
-            <Col xs={12} md={6} lg={5}>
-              <h2 className="mb-3">Enter OTP</h2>
-              <Form className="form" onSubmit={this.onSubmitOtp}>
-                <Form.Group>
-                  <Form.Control
-                    id="otp"
-                    type="number"
-                    name="otp"
-                    placeholder="OTP"
-                    onChange={this.onChangeHandler}
-                  />
-                </Form.Group>
-                <button  type="submit"> Submit</button>
-              </Form>
-            </Col>
-          </Row>
-         {/* <GoogleLogin /> */}
-        </Container>
+        <input type="number" onChange={(e)=>this.setState({phonenumber:e.target.value})}/>
       </div>
-    );
-  }
+      <div>
+        <input type="button" value="send" onClick={()=>this.onSignInSubmit()}/>
+      </div>
+    </div>
+  );
 }
+}
+
+
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,16 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+import firebase from 'firebase'
+
+var firebaseConfig = {
   apiKey: "AIzaSyB1zkafcglGkRlsds47H10CIwSsKtnjsqE",
   authDomain: "healthscore-4fcdf.firebaseapp.com",
   databaseURL: "https://healthscore-4fcdf-default-rtdb.firebaseio.com",
   projectId: "healthscore-4fcdf",
   storageBucket: "healthscore-4fcdf.appspot.com",
   messagingSenderId: "959287164059",
-  appId: "1:959287164059:web:e93bda74c7b194c0559a3e",
-  measurementId: "G-QDM2S2KJ3N"
-};
+  appId: "1:959287164059:android:99fbe625426c70e8559a3e",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
+
